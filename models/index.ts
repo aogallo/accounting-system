5f72d6078108bd5853d5dd09c9400ac83ab2c669
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-05-14 21:59:13 -0600

    remove IUser interface

--- models/index.ts
+++ models/index.ts
@@ -1,13 +1,13 @@
 import { model, models } from 'mongoose'
-import userSchema, { IUser } from './User'
+import userSchema from './User'
 import invoiceSchema from './Invoice'
 import companySchema from './Company'
 import accountSchema from './Account'
-import { Account, Company, Invoice } from '@/app/lib/definitions'
+import { Account, Company, Invoice, User } from '@/app/lib/definitions'
 
 export const InvoiceModel =
   models.Invoice || model<Invoice>('Invoice', invoiceSchema)
-export const UserModel = models.User || model<IUser>('User', userSchema)
+export const UserModel = models.User || model<User>('User', userSchema)
 export const CompanyModel =
   models.Company || model<Company>('Company', companySchema)
 export const AccountModel =
